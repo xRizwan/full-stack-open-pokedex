fdc314989a87c84bebdef67c4ab9470a599effe1
Rizwan <xrizwanr@gmail.com>
2021-03-01 20:38:17 +0500

    removed semicolons

--- cypress/integration/pokemon_page.js
+++ cypress/integration/pokemon_page.js
@@ -1,7 +1,7 @@
 describe('CharizardEntry', function () {
   it('Entry page contains correct values', function() {
-    cy.visit('http://localhost:5000/');
+    cy.visit('http://localhost:5000/')
     cy.contains('charizard').click()
-    cy.contains('solar power');
+    cy.contains('solar power')
   })
 })
